b1623eabb1a867e737e49bd341e4bc91a0082dc9
Maid Sabanovic <maid.sabanovic@gws.ms>
2023-08-25 17:11:56 +0200

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ebook-website';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    // Initial check for scroll position
+    this.handleScroll();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event): void {
+    this.handleScroll();
+  }
+
+  private handleScroll(): void {
+    const navbar = document.querySelector('.navbar');
+    if (navbar) {
+      if (window.scrollY > 50) {
+        navbar.classList.add('bg-dark');
+        navbar.classList.add('navbar-sticky');
+      } else {
+        navbar.classList.remove('bg-dark');
+        navbar.classList.remove('navbar-sticky');
+      }
+    }
+  }
 }
